947a9b75014996592ad577782a80af51de22e6d4
robinvanpe <marcoscaraballo32@gmail.com>
2022-04-14 22:47:46 -0300

    144222247

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,13 +10,13 @@ const nuevoNum = '7';
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 420;
 
 
 // En los próximos 22 problemas, deberás completar la función.
